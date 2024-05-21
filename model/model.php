3953e5be33fb9db328cb0ac31a05c142a6d6a4fc
Fireboss_05 <47520445+Fireboss05@users.noreply.github.com>
2024-05-21 14:57:00 +0200

    book by id

--- model/model.php
+++ model/model.php
@@ -23,43 +23,54 @@ class Model {
 	}
 
 	/**
-     * Execute an SQL request.
-     *
-     * @param string $requete The SQL query.
-     * @param array $params The parameters for the query.
-     * @return array The result set.
-     */
+	 * Execute an SQL request.
+	 *
+	 * @param string $requete The SQL query.
+	 * @param array $params The parameters for the query.
+	 * @return array The result set.
+	 */
 	public function executeRequest(string $requete, array $params = []) {
-        try {
-            $stmt = self::$db->prepare($requete);
-            $stmt->execute($params);
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        } catch (Exception $e) {
-            die('Erreur : ' . $e->getMessage());
-        }
-    }
+		try {
+			$stmt = self::$db->prepare($requete);
+			$stmt->execute($params);
+			return $stmt->fetchAll(PDO::FETCH_ASSOC);
+		} catch (Exception $e) {
+			die('Erreur : ' . $e->getMessage());
+		}
+	}
 
 	/**
-     * Get all books from the database.
-     *
-     * @return array The list of books.
-     */
+	 * Get all books from the database.
+	 *
+	 * @return array The list of books.
+	 */
 	public function getAllBooks() {
-        $requete = "SELECT * FROM BOOK";
-        return $this->executeRequest($requete);
-    }
+		$requete = "SELECT * FROM BOOK";
+		return $this->executeRequest($requete);
+	}
 
 	/**
-     * Search for a book by name.
-     *
-     * @param string $nom The name of the book.
-     * @return array The list of books that match the name.
-     */
+	 * Search for a book by name.
+	 *
+	 * @param string $id The name of the book.
+	 * @return array The book that match the name.
+	 */
+	public function getBookById($id) {
+		$requete = "SELECT * FROM BOOK WHERE 'id' = ".$id."";
+		return ($this->executeRequest($requete))[0];
+	}
+
+	/**
+	 * Search for a book by name.
+	 *
+	 * @param string $nom The name of the book.
+	 * @return array The list of books that match the name.
+	 */
 	public function searchBookByName(string $nom) {
-        $requete = "SELECT * FROM BOOK WHERE name LIKE :name";
-        $params = ['name' => '%' . $nom . '%'];
-        return $this->executeRequest($requete, $params);
-    }
+		$requete = "SELECT * FROM BOOK WHERE name LIKE :name";
+		$params = ['name' => '%' . $nom . '%'];
+		return $this->executeRequest($requete, $params);
+	}
 
 
 	// === Il faut coder ici ===
