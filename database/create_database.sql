012299e1f10a251f3d918cf61e6f729048117540
loic <loic.plasse@etu.univ-lyon1.fr>
2024-05-21 15:19:17 +0200

    Fixed de structure BDD

--- database/create_database.sql
+++ database/create_database.sql
@@ -2,6 +2,25 @@
 CREATE DATABASE IF NOT EXISTS `bibliotheque_agile`;
 USE `bibliotheque_agile`;
 
+
+CREATE TABLE Category(
+     id INT,
+     name VARCHAR(64) NOT NULL,
+     PRIMARY KEY(id),
+     UNIQUE(name)
+);
+
+CREATE TABLE Customer(
+    id INT,
+    first_name VARCHAR(64) NOT NULL,
+    last_name VARCHAR(64) NOT NULL,
+    birth_date DATE NOT NULL,
+    phone INT NOT NULL,
+    email VARCHAR(64) NOT NULL,
+    password VARCHAR(64) NOT NULL,
+    is_admin int NOT NULL,
+    PRIMARY KEY(id)
+);
 -- transaction table
 CREATE TABLE Book(
     id INT,
@@ -17,21 +36,5 @@ CREATE TABLE Book(
     FOREIGN KEY(borrowed_by) REFERENCES Customer(id)
 );
 
-CREATE TABLE Customer(
-    id INT,
-    first_name VARCHAR(64) NOT NULL,
-    last_name VARCHAR(64) NOT NULL,
-    birth_date DATE NOT NULL,
-    phone INT NOT NULL,
-    email VARCHAR(64) NOT NULL,
-    password VARCHAR(64) NOT NULL,
-    is_admin LOGICAL NOT NULL,
-    PRIMARY KEY(id)
-);
 
-CREATE TABLE Category(
-    id INT,
-    name VARCHAR(64) NOT NULL,
-    PRIMARY KEY(id),
-    UNIQUE(name)
-);
+
