aec549fede0d7fd31db3cb7e519a9230e0a002dd
Antonin Slv <antonin.sylvestre@orange.fr>
2024-05-22 16:38:31 +0200

    placement élem au bon endroit

--- controller/registerController.php
+++ controller/registerController.php
@@ -10,10 +10,30 @@ class RegisterController extends Controller {
 
 	// === Methods ===
 
+	public function RegisterController() {
+		parent::__construct();
+		if (isset($_POST["action"])) {
+            if ($_POST["action"] == "registerClient") {
+                $this->registerClient();
+            }
+        }
+	}
+
 	/**
 	 * Renders the register page.
 	 */
 	public function render() {
+
 		require_once 'view/register.php';
 	}
+	private function registerClient() {
+        $nom = $_POST["nom"];
+        $prenom = $_POST["prenom"];
+        $date_naissance = $_POST["date_naissance"];
+        $telephone = $_POST["telephone"];
+        $email = $_POST["email"];
+        $adresse = $_POST["adresse"];
+
+        $this->model->registerClient($nom, $prenom, $date_naissance, $telephone, $email, $adresse);
+    }
 }
