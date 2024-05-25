1cd83ece0c5d099d964fbb10a2fb7ce84395c155
loic <loic.plasse@etu.univ-lyon1.fr>
2024-05-25 13:54:09 +0200

    modificat de la requête par catégorie

--- controller/home.php
+++ controller/home.php
@@ -14,15 +14,13 @@ class HomeController extends Controller {
 	 * Renders the home page.
 	 */
     public function render() {
-        // Obtenez toutes les catégories
         $categories = $this->model->getAllCategories();
 
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            // Récupérer le titre du livre et l'ID de catégorie depuis le formulaire
+
             $title = isset($_POST['book_name']) ? trim($_POST['book_name']) : '';
             $idCategory = isset($_POST['categorie-select']) ? trim($_POST['categorie-select']) : '';
 
-            // Initialiser la variable des livres
             $books = [];
 
             if (!empty($title)) {
@@ -33,17 +31,14 @@ class HomeController extends Controller {
                 $books = $this->model->getBooksByCategory($idCategory);
             }
 
-            // Si aucune recherche n'a été effectuée, obtenir tous les livres
             if (empty($books)) {
                 $books = $this->model->getAllBooks();
             }
 
         } else {
-            // Si ce n'est pas une requête POST, obtenir tous les livres
             $books = $this->model->getAllBooks();
         }
 
-        // Inclure la vue home.php avec les livres et les catégories
         require_once 'view/home.php';
     }
 
