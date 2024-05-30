259968b2936ab4757e1339b8d7cde6a1b14a944e
JulienSlvt <julien.salvat2003@gmail.com>
2024-05-30 12:52:02 +0200

    Page réservation

--- view/reservation.php
+++ view/reservation.php
@@ -1,18 +1,29 @@
 <?php ob_start(); ?>
 
 <main id="reservation">
-	<form action="index.php?action=reservation&id=<?= $bookId ?>" method="post">
-		<input type="email" name="email" placeholder="email@example.com" required>
-        <input type="text" name="firstname" placeholder="Prénom" required>
-		<input type="text" name="lastname" placeholder="Nom" required>
-		<input type="date" name="birthdate" placeholder="Date de naissance" required>
-		<input type="phone" name="phone" placeholder="Numéro de téléphone" required>
-		<input type="submit" name="submit" value="Réserver">
+    <h1>Réservation de livre</h1>
+    <form action="index.php?action=reservation&id=<?= $bookId ?>" method="post">
+        <label for="email">Email :</label>
+        <input type="email" id="email" name="email" placeholder="email@example.com" required>
+
+        <label for="firstname">Prénom :</label>
+        <input type="text" id="firstname" name="firstname" placeholder="Prénom" required>
+
+        <label for="lastname">Nom :</label>
+        <input type="text" id="lastname" name="lastname" placeholder="Nom" required>
+
+        <label for="birthdate">Date de naissance :</label>
+        <input type="date" id="birthdate" name="birthdate" required>
+
+        <label for="phone">Numéro de téléphone :</label>
+        <input type="tel" id="phone" name="phone" placeholder="Numéro de téléphone" required>
+
+        <input type="submit" name="submit" value="Réserver">
     </form>
 </main>
 
 <?php
 $content = ob_get_clean();
 
-
-require_once 'view/layout/layout.php'; ?>
+require_once 'view/layout/layout.php';
+?>
